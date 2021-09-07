c550da304b38ec39d4b5e9b2a035f8374fcdb9e2
admin <admin@Mac-mini-GhulJr.local>
2021-09-07 20:53:07 +0200

    Build: use mock modules

---
+++ OneHabit-tools-android/src/main/java/com/ghuljr/onehabit_tools_android/FirebaseModule.kt
@@ -0,0 +1,7 @@
+package com.ghuljr.onehabit_tools_android
+
+import org.koin.dsl.module
+
+val firebaseModule = module {
+
+}
--- app/src/main/java/com/ghuljr/onehabit/App.kt
+++ app/src/main/java/com/ghuljr/onehabit/App.kt
@@ -2,6 +2,9 @@ package com.ghuljr.onehabit
 
 import android.app.Application
 import androidx.annotation.CallSuper
+import com.ghuljr.onehabit_presenter.dependency_injection.dataModule
+import com.ghuljr.onehabit_presenter.dependency_injection.presenterModule
+import com.ghuljr.onehabit_tools_android.firebaseModule
 import com.ghuljr.onehabit_tools_android.helper.FirebaseConfigHelper
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -16,7 +19,7 @@ class App : Application() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules()
+            modules(presenterModule, dataModule, firebaseModule)
         }
 
         FirebaseConfigHelper.init(this)
