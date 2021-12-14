8079f0a74a8b0f8f29380d5ab82263d6252701f6
admin <admin@Mac-mini-GhulJr.local>
2021-12-14 23:06:08 +0100

    Build: setup intro activity

---
+++ app/src/main/java/com/ghuljr/onehabit/ui/intro/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.ghuljr.onehabit.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ghuljr.onehabit.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
