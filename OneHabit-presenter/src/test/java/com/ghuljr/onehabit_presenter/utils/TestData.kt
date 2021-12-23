cda56402ce8e60d8d3671336616b0fb694af9793
ghuljr <oskarrek98@gmail.com>
2021-12-23 18:49:47 +0100

    Test: password validator

--- OneHabit-presenter/src/test/java/com/ghuljr/onehabit_presenter/utils/TestData.kt
+++ OneHabit-presenter/src/test/java/com/ghuljr/onehabit_presenter/utils/TestData.kt
@@ -5,4 +5,8 @@ object TestData {
     const val EMPTY_FIELD = ""
     const val EMAIL_INVALID = "email@example"
     const val EMAIL_VALID = "email@example.com"
+    const val PASSWORD_TO_SHORT = "123456"
+    const val PASSWORD_ONLY_LOWER_CASE = "haslo1234"
+    const val PASSWORD_WHITE_SPACE= "haslo 1234"
+    const val PASSWORD_VALID = "H@slO123"
 }
