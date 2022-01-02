d30e5fd38d34f3cf751a8b8f2bc9f69184fbb180
ghuljr <oskarrek98@gmail.com>
2022-01-02 19:16:04 +0100

    Build: create bottom nav bar

--- app/src/main/java/com/ghuljr/onehabit/ui/main/MainActivity.kt
+++ app/src/main/java/com/ghuljr/onehabit/ui/main/MainActivity.kt
@@ -2,6 +2,10 @@ package com.ghuljr.onehabit.ui.main
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ghuljr.onehabit.R
 import com.ghuljr.onehabit.databinding.ActivityMainBinding
 import com.ghuljr.onehabit.ui.base.BaseActivity
 import com.ghuljr.onehabit.ui.intro.change_data.FillUserDataActivity
@@ -9,6 +13,13 @@ import com.ghuljr.onehabit_presenter.main.MainPresenter
 import com.ghuljr.onehabit_presenter.main.MainView
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainView, MainPresenter>(), MainView {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBind.bottomNavigation.setupWithNavController((supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController)
+    }
 
     override fun getPresenterView(): MainView = this
     override fun bindView(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
