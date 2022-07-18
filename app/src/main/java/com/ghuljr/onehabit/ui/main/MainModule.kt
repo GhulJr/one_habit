73c9f57f298b003b4716df99fdf39a5d13c2cea3
Oskar Rek <oskarrek98@gmail.com>
2022-07-18 18:51:02 +0200

    Feat: display today framgnet

--- app/src/main/java/com/ghuljr/onehabit/ui/main/MainModule.kt
+++
@@ -1,7 +0,0 @@
-package com.ghuljr.onehabit.ui.main
-
-import com.ghuljr.onehabit_tools_android.base.di.BaseActivityModule
-import dagger.Module
-
-@Module
-class MainModule : BaseActivityModule<MainActivity>()
