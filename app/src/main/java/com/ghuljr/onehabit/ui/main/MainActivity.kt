6b08d2211ee319a8f3c8082d537c6707c872b0f8
Oskar Rek <oskarrek98@gmail.com>
2022-07-20 20:06:57 +0200

    Feat: prepare bottom navigation

--- app/src/main/java/com/ghuljr/onehabit/ui/main/MainActivity.kt
+++ app/src/main/java/com/ghuljr/onehabit/ui/main/MainActivity.kt
@@ -2,6 +2,11 @@ package com.ghuljr.onehabit.ui.main
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ghuljr.onehabit.R
 import com.ghuljr.onehabit.databinding.ActivityMainBinding
 import com.ghuljr.onehabit.ui.base.BaseActivity
 import com.ghuljr.onehabit.ui.intro.change_data.FillUserDataActivity
@@ -12,6 +17,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainView, MainPresenter>(
 
     override fun getPresenterView(): MainView = this
     override fun bindView(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBind.bottomNavigation.setupWithNavController(navController)
+    }
 
     override fun redirectToFillRemainingData() {
         startActivity(FillUserDataActivity.newIntent(this))
