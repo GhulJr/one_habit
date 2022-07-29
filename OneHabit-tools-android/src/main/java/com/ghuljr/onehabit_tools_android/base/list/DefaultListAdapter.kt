59a3846efea5494f2dd3e2ff7e78ee491bac6371
Oskar Rek <oskarrek98@gmail.com>
2022-07-29 18:10:32 +0200

    Feat: create item manager

--- OneHabit-tools-android/src/main/java/com/ghuljr/onehabit_tools_android/base/list/DefaultListAdapter.kt
+++ OneHabit-tools-android/src/main/java/com/ghuljr/onehabit_tools_android/base/list/DefaultListAdapter.kt
@@ -3,4 +3,6 @@ package com.ghuljr.onehabit_tools_android.base.list
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class DefaultListAdapter<ITEM: UniqueItem<ITEM>, HOLDER: RecyclerView.ViewHolder> : ListAdapter<ITEM, HOLDER>(DefaultItemCallback<ITEM>())
+abstract class DefaultListAdapter<ITEM: UniqueItem<ITEM>, HOLDER: RecyclerView.ViewHolder> : ListAdapter<ITEM, HOLDER>(DefaultItemCallback<ITEM>()) {
+
+}
