6a6bfa2a1e5a22aa72bbc339e8a5443b3d6a0885
Oskar Rek <oskarrek98@gmail.com>
2022-08-24 21:40:40 +0200

    Feat: create timestamp separator

--- OneHabit-tools-android/src/main/java/com/ghuljr/onehabit_tools_android/widget/TextDivider.kt
+++ OneHabit-tools-android/src/main/java/com/ghuljr/onehabit_tools_android/widget/TextDivider.kt
@@ -1,6 +1,7 @@
 package com.ghuljr.onehabit_tools_android.widget
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -14,27 +15,32 @@ import com.ghuljr.onehabit_tools_android.databinding.WidgetTextDividerBinding
 //TODO: add animations
 class TextDivider(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs, 0, 0) {
 
-    private val viewBind by lazy { WidgetTextDividerBinding.inflate(LayoutInflater.from(context),  this, true) }
+    private val viewBind by lazy { WidgetTextDividerBinding.inflate(LayoutInflater.from(context), this, true) }
 
-    private val paint: Paint = Paint().apply {
-        color = ContextCompat.getColor(context, R.color.primary_black)
-    }
+    private val paint: Paint by lazy { Paint().apply { color = layoutColor } }
 
+    private val layoutColor: Int
     private val textPadding: Float
 
     init {
-        context.theme.obtainStyledAttributes(attrs, R.styleable.OneHabit_Widget_TextDivider, 0, 0).apply {
-            textPadding = getDimension(R.styleable.OneHabit_Widget_TextDivider_textPadding, 0f)
+        context.theme.obtainStyledAttributes(attrs, R.styleable.OneHabit_Widget_TextDivider, 0, 0)
+            .apply {
+                layoutColor = getColor(R.styleable.OneHabit_Widget_TextDivider_color, ContextCompat.getColor(context, R.color.primary_black))
+                textPadding = getDimension(R.styleable.OneHabit_Widget_TextDivider_textPadding, 0f)
 
-            viewBind.apply {
-                title.text = getString(R.styleable.OneHabit_Widget_TextDivider_android_text)
-                title.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimension(R.styleable.OneHabit_Widget_TextDivider_textSize, resources.getDimension(R.dimen.default_text_size_large)))
-            }
+                viewBind.apply {
+                    title.apply {
+                        text = getString(R.styleable.OneHabit_Widget_TextDivider_android_text)
+                        setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimension(R.styleable.OneHabit_Widget_TextDivider_textSize, resources.getDimension(R.dimen.default_text_size_large)))
+                        setTextColor(ColorStateList.valueOf(layoutColor))
+                    }
 
-            paint.apply {
-                strokeWidth = getDimension(R.styleable.OneHabit_Widget_TextDivider_strokeWidth, 0f)
+                }
+
+                paint.apply {
+                    strokeWidth = getDimension(R.styleable.OneHabit_Widget_TextDivider_strokeWidth, 0f)
+                }
             }
-        }
     }
 
 
