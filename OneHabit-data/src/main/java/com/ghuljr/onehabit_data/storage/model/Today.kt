03877d1ae6c2ba1a361e5417e58e40641364de61
Oskar Rek <oskarrek98@gmail.com>
2022-09-12 20:48:14 +0200

    Feat: today model

--- OneHabit-data/src/main/java/com/ghuljr/onehabit_data/storage/model/Today.kt
+++ OneHabit-data/src/main/java/com/ghuljr/onehabit_data/storage/model/Today.kt
@@ -1,4 +1,21 @@
 package com.ghuljr.onehabit_data.storage.model
 
-class Today {
+import com.ghuljr.onehabit_data.base.storage.BaseEntity
+
+data class Today(
+    val milestoneId: Int,
+    val orderNumber: Int,
+    val todayMotto: String,
+    val actions: List<Action>
+) : BaseEntity() {
+
+    /*TODO: create properties to calculate score based on actions calculation
+    * val totalScore: Int
+    * val finishState: FinishState
+    *  */
+
+    // TODO: it might be only needed for visual model
+    enum class FinishState {
+        SUCCESS, REQUIRES_ACTION, FAILED
+    }
 }
