a94b2f9ee879f92a7817d2c7ffd3b57e4a2fb214
Oskar Rek <oskarrek98@gmail.com>
2022-09-28 17:10:10 +0200

    Feat: display manu for action

--- app/src/main/java/com/ghuljr/onehabit/ui/create_habit/CreateHabitActivity.kt
+++ app/src/main/java/com/ghuljr/onehabit/ui/create_habit/CreateHabitActivity.kt
@@ -2,19 +2,48 @@ package com.ghuljr.onehabit.ui.create_habit
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import com.ghuljr.onehabit.R
 import com.ghuljr.onehabit.databinding.ActivityCreateHabitBinding
 import com.ghuljr.onehabit.ui.base.BaseActivity
 import com.ghuljr.onehabit_presenter.create_habit.CreateHabitPresenter
 import com.ghuljr.onehabit_presenter.create_habit.CreateHabitView
 
-class CreateHabitActivity : BaseActivity<ActivityCreateHabitBinding, CreateHabitView, CreateHabitPresenter>(), CreateHabitView {
+class CreateHabitActivity :
+    BaseActivity<ActivityCreateHabitBinding, CreateHabitView, CreateHabitPresenter>(),
+    CreateHabitView {
 
 
-    override fun bindView(): ActivityCreateHabitBinding = ActivityCreateHabitBinding.inflate(layoutInflater)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBind.apply {
+            val popupMenu = PopupMenu(this@CreateHabitActivity, habitAction).apply {
+                inflate(R.menu.menu_habit_action)
+                setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.eat -> true
+                        R.id.not_eat -> true
+                        R.id.train -> true
+                        R.id.start_doing -> true
+                        R.id.stop_doing -> true
+                        else -> false
+                    }
+                }
+            }
+            habitAction.setOnClickListener { popupMenu.show() }
+        }
+    }
+
+    override fun bindView(): ActivityCreateHabitBinding =
+        ActivityCreateHabitBinding.inflate(layoutInflater)
+
     override fun getPresenterView(): CreateHabitView = this
 
+    override fun handleCurrentStep(currentStep: CreateHabitPresenter.Step) {
+        // TODO: handle visibility of incoming views
+    }
 
     companion object {
         fun intent(from: Context) = Intent(from, CreateHabitActivity::class.java)
--- app/src/main/java/com/ghuljr/onehabit/ui/main/today/list/ActionItemsExtensions.kt
+++ app/src/main/java/com/ghuljr/onehabit/ui/main/today/list/ActionItemsExtensions.kt
@@ -6,10 +6,10 @@ import com.ghuljr.onehabit_tools.model.HabitTopic
 
 fun HabitTopic.generateTitle(resources: Resources, subject: String, exceeded: Boolean = false) = resources.run {
     when(this@generateTitle) {
-        HabitTopic.EAT -> getString(if(exceeded) R.string.eat else R.string.eat_opposite, subject)
-        HabitTopic.NOT_EAT -> getString(if(exceeded)R.string.not_eat else  R.string.not_eat_opposite, subject)
-        HabitTopic.TRAIN -> getString(if(exceeded) R.string.train_opposite else R.string.train, subject)
-        HabitTopic.START_DOING -> getString(if(exceeded) R.string.start_doing_opposite else R.string.start_doing, subject)
-        HabitTopic.STOP_DOING -> getString(if(exceeded) R.string.stop_doing_opposite else R.string.stop_doing, subject)
+        HabitTopic.EAT -> getString(if(exceeded) R.string.eat_param else R.string.eat_opposite, subject)
+        HabitTopic.NOT_EAT -> getString(if(exceeded)R.string.not_eat_param else  R.string.not_eat_opposite, subject)
+        HabitTopic.TRAIN -> getString(if(exceeded) R.string.train_opposite else R.string.train_param, subject)
+        HabitTopic.START_DOING -> getString(if(exceeded) R.string.start_doing_opposite else R.string.start_doing_param, subject)
+        HabitTopic.STOP_DOING -> getString(if(exceeded) R.string.stop_doing_opposite else R.string.stop_doing_param, subject)
     }
 }
