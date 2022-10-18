770e98c1c660f29eb9d2630395132416bdcc4829
Oskar Rek <oskarrek98@gmail.com>
2022-10-18 18:12:46 +0200

    Feat: use this in code

--- app/src/main/java/com/ghuljr/onehabit/ui/generate_milestone/finish_habit/FinishHabitFragment.kt
+++ app/src/main/java/com/ghuljr/onehabit/ui/generate_milestone/finish_habit/FinishHabitFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import arrow.core.Option
+import com.ghuljr.onehabit.R
 import com.ghuljr.onehabit.databinding.FragmentFinishHabitBinding
 import com.ghuljr.onehabit.ui.base.BaseFragment
+import com.ghuljr.onehabit_error.BaseEvent
+import com.ghuljr.onehabit_error_android.event_handler.EventHandler
+import com.ghuljr.onehabit_error_android.event_manager.SnackbarEventManager
 import com.ghuljr.onehabit_presenter.generate_milestone.finish_habit.FinishHabitPresenter
 import com.ghuljr.onehabit_presenter.generate_milestone.finish_habit.FinishHabitView
+import com.google.android.material.snackbar.Snackbar
 
 class FinishHabitFragment : BaseFragment<FragmentFinishHabitBinding, FinishHabitView, FinishHabitPresenter>(), FinishHabitView {
 
@@ -25,4 +31,18 @@ class FinishHabitFragment : BaseFragment<FragmentFinishHabitBinding, FinishHabit
     ): FragmentFinishHabitBinding = FragmentFinishHabitBinding.inflate(layoutInflater, container, false)
 
     override fun getPresenterView(): FinishHabitView = this
+
+    override fun handleEvent(event: Option<BaseEvent>) {
+        val eventManager = EventHandler(listOf(SnackbarEventManager(
+            eventView = viewBind.root,
+            duration = Snackbar.LENGTH_INDEFINITE,
+            actionWithName = { } to getString(R.string.ok)
+        )), viewBind.loadingIndicator)
+
+        eventManager(event)
+    }
+
+    override fun finish() {
+        requireActivity().finish()
+    }
 }
